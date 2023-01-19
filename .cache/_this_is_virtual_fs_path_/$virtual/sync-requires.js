202bcfadf3198d77730cdfceb6b1da9ee70ca102
PickleEric <60155510+PickleEric@users.noreply.github.com>
2023-01-19 14:13:22 -0600

    created about me page

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Erudz\\Documents\\Worldbuilderapp\\worldbuilderapp\\.cache\\dev-404-page.js"))
+}
+
