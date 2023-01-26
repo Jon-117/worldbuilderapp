b2495438f0beef3a81d166be8b84bcdfb0ebb9b1
JonK <jkonoch@gmail.com>
2023-01-26 09:49:54 -0600

    Slight changes to api interaction file and adding line to .gitignore for local testing folder "testing_IGNORE"

--- src/api_interaction.py
+++ src/api_interaction.py
@@ -1,17 +1,23 @@
 import os
 import openai
+
 openai.api_key = os.getenv("OPEN_AI_KEY")
 
 while True:
     prompt = input("Talk to Davinci003: \n")
 
     response_raw = openai.Completion.create(
-        model = "text-davinci-003",
-        prompt = prompt,
-        max_tokens = 1000,
-        temperature = 0
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=10,
+        temperature=0
     )
 
-    # response = response_raw["text"]
+    response = response_raw["choices"][0]['text']
 
-    print(response_raw)
+
+
+    # print(response_raw)
+    print(response)
+
+
