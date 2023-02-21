0290e1cd1e3b17815bf88f1214a02f9b430a21bf
PickleEric <60155510+PickleEric@users.noreply.github.com>
2023-02-21 15:21:15 -0600

    added link array from index.js

--- Worldbuilder/src/pages/navbar.js
+++ Worldbuilder/src/pages/navbar.js
@@ -25,5 +25,26 @@ const Navbar = () => {
     </nav>
   )
 }
-
+<p className={styles.intro}>
+        <b>Example pages:</b>{" "}
+        {samplePageLinks.map((link, i) => (
+          <React.Fragment key={link.url}>
+            <Link to={link.url}>{link.text}</Link>
+            {i !== samplePageLinks.length - 1 && <> · </>}
+          </React.Fragment>
+        ))}
+const pageLinks = [
+    {
+      text: "Page 2",
+      url: "page-2",
+      badge: false,
+      description:
+        "A simple example of linking to another page within a Gatsby site",
+    },
+    { text: "TypeScript", url: "using-typescript" },
+    { text: "Server Side Rendering", url: "using-ssr" },
+    { text: "Deferred Static Generation", url: "using-dsg" },
+    { text: "Profile", url: "profile"},
+    { text: "Login", url: "login"}
+  ]
 export default Navbar
